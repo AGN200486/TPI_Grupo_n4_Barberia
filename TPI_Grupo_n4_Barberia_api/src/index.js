40f36c5a791f4da95fec9196e25d4d24cac8aefe
AGN200486 <vellaagustin04@gmail.com>
2026-05-12 14:26:43 -0300

    12/5/26 Agregacion de componentes en API

--- TPI_Grupo_n4_Barberia_api/src/index.js
+++ TPI_Grupo_n4_Barberia_api/src/index.js
@@ -1,8 +1,26 @@
-import express from 'express';
-
-import { PORT } from './config.js';
+import express from "express";
+import { PORT } from "./config.js";
+import productRoutes from "./routes/books.routes.js";
+import { sequelize } from "./db.js";
+import "./models/Product.js";
 
 const app = express();
 
-app.listen(PORT);
-console.log("Server listening on port ${PORT}") 
+try {
+  app.use(express.json());
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
+  app.use(productRoutes);
+
+  await sequelize.sync();
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+} catch (error) {
+  console.error("There was an error on initialization:", error);
+}
